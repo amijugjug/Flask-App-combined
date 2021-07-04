3712fbf845c265603ee61f2f5e362eb787dd0a21
Prakhar Srivastava <sriprakhar2@gmail.com>
2021-07-04 09:24:06 +0530

    Add files via upload

---
+++ flask_blog/errors/__init__.py
@@ -0,0 +1,1 @@
+# This file is used to make this directory as package please do not delete it.
---
+++ flask_blog/main/__init__.py
@@ -0,0 +1,2 @@
+# This file is used to make this directory as package please do not delete it.
+# This package contains the routes for home and about pages.
